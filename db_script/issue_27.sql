49240fad1635a548e631ea1ebd699b471fa3bebe
jkbngl <32094260+jkbngl@users.noreply.github.com>
2019-05-09 21:26:49 +0200

    #27 changed column data type to boolean

--- db_script/issue_27.sql
+++ db_script/issue_27.sql
@@ -2,4 +2,10 @@ select  *
 from    tp_preferences;
 
 
-ALTER TABLE tp_preferences add check_chart_type integer DEFAULT 0;
+ALTER TABLE tp_preferences
+  drop COLUMN check_chart_type;
+  
+
+ALTER TABLE tp_preferences
+  add COLUMN check_chart_type boolean DEFAULT false NOT NULL
+  

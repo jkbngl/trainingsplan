476d933a0705bf6f7918032915d07eee5a6c3a4f
jkbngl <32094260+jkbngl@users.noreply.github.com>
2019-06-06 21:53:01 +0200

    #37 set all notes to empty to fix issue with displaying empty notes wrong

--- db_script/issue_37.sql
+++ db_script/issue_37.sql
@@ -1,2 +1,8 @@
 ALTER TABLE tp_exercise
 ADD COLUMN note character  varying(1000);
+
+update tp_exercise set deprecated = 0 where changed > NOW() - INTERVAL '1 DAY' and deprecated = 2;
+
+update tp_exercise set note = '' where note is null;
+
+select * from tp_exercise order by id desc;

9c210efdbd4e1aae4f322c7da2f0a34a4569d9a6
jkbngl <jakob.engl@hotmail.de>
2019-06-10 20:12:59 +0200

    #76 first working example of a intellj rest backend

---
+++ backend/IdeaProjects/wf15restfultpbackend/src/main/java/hw/APIApplication.java
@@ -0,0 +1,17 @@
+package hw;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class APIApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<>();
+        set.add( HelloWorldResource.class );
+        return set;
+    }
+}

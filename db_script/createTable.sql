fb047d2bc81e64e5536ee3a47f1547ca5f0a3633
jkbngl <32094260+jkbngl@users.noreply.github.com>
2019-06-18 21:38:24 +0200

    #67 updated create table script

--- db_script/createTable.sql
+++ db_script/createTable.sql
@@ -61,3 +61,17 @@ CREATE TABLE public.tp_preferences (
     check_chart_type boolean DEFAULT false NOT NULL,
     check_dialog_save boolean DEFAULT true NOT NULL
 );
+
+CREATE TABLE tp_bm_it (
+    id SERIAL NOT NULL PRIMARY KEY,
+    userid_fk integer NOT NULL,
+    valuename character varying(100) NOT NULL,
+    uom character varying(20) NOT NULL,
+    timeofday character varying(20),
+    value integer DEFAULT 1,
+    note character varying(1000),
+    base_stat integer DEFAULT 0,
+    referenced_stat integer DEFAULT 0,
+    created timestamp without time zone DEFAULT now(),
+    changed timestamp without time zone DEFAULT now()
+);

959e99ffaa12bfc0e2b9a4b53c57d8b042d55005
jkbngl <32094260+jkbngl@users.noreply.github.com>
2019-06-18 21:57:02 +0200

    #67 changed bm table to snake case and added bm to column names

--- db_script/createTable.sql
+++ db_script/createTable.sql
@@ -65,13 +65,13 @@ CREATE TABLE public.tp_preferences (
 CREATE TABLE tp_bm_it (
     id SERIAL NOT NULL PRIMARY KEY,
     userid_fk integer NOT NULL,
-    valuename character varying(100) NOT NULL,
+    value_name character varying(100) NOT NULL,
     uom character varying(20) NOT NULL,
-    timeofday character varying(20),
+    time_of_day character varying(20),
     value integer DEFAULT 1,
     note character varying(1000),
-    base_stat integer DEFAULT 0,
-    referenced_stat integer DEFAULT 0,
+    base_bm_value integer DEFAULT 0,
+    referenced_bm_value integer DEFAULT 0,
     created timestamp without time zone DEFAULT now(),
     changed timestamp without time zone DEFAULT now()
 );

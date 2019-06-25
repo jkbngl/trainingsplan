d8c244fe70378f46ce645eb333d2b63dbee5fc29
jkbngl <jakob.engl@hotmail.de>
2019-06-25 22:12:58 +0200

    Revert "#76 moved final project out of other files"
    This reverts commit ab080e5a67a483ff21efbd1f951ed9c1a3256f86.

---
+++ backend/IdeaProjects/wf15restfultpbackend/src/main/java/tp_backend/APIApplication.java
@@ -0,0 +1,17 @@
+package tp_backend;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class APIApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<>();
+        set.add( DBConnector.class );
+        return set;
+    }
+}

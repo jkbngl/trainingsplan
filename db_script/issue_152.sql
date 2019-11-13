21ebacdc10620d124e69d2d672cb1db353a7b377
Jakob Engl <32094260+jkbngl@users.noreply.github.com>
2019-11-13 22:34:27 +0100

    #152 updated to work with plans as well

--- db_script/issue_152.sql
+++ db_script/issue_152.sql
@@ -8,10 +8,13 @@ select  e.id
 from    tp_exercise e
 join    tp_day d on e.day_fk = d.id
 join    tp_plan p on d.plan_fk = p.id
-where   base_ex = 748
+where   p.id = 149
+and     base_ex = 748
 and     e.created < now()
 order by e.created desc
 fetch first row only
 ;
 
-select * from tp_exercise;
+select * from tp_exercise where id = 748;
+select * from tp_day where id = 307;
+select * from tp_plan where id = 149;

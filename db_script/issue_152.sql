2c8cf07bf239f2108a013b47a04cbdba33a34eb4
Jakob Engl <32094260+jkbngl@users.noreply.github.com>
2019-11-13 22:40:44 +0100

    #152 test for with all ex per plant

--- db_script/issue_152.sql
+++ db_script/issue_152.sql
@@ -9,11 +9,26 @@ from    tp_exercise e
 join    tp_day d on e.day_fk = d.id
 join    tp_plan p on d.plan_fk = p.id
 where   p.id = 149
-and     base_ex = 748
+--and     base_ex = 748
 and     e.created < now()
 order by e.created desc
 fetch first row only
 ;
+
+
+SELECT  MIN(x.id)
+      , x.base_ex
+      , x.weight
+FROM    tp_exercise x
+JOIN (SELECT  p.base_ex
+            , MAX(weight) AS weight
+      FROM    tp_exercise p
+      GROUP BY p.base_ex) y ON y.base_ex = x.base_ex
+                           AND y.weight = x.weight
+GROUP BY x.base_ex
+       , x.weight
+;
+
 
 select * from tp_exercise where id = 748;
 select * from tp_day where id = 307;
